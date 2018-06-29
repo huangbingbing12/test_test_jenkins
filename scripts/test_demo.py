8a74ac389e50a5f079b873e0db3d5dcf5b78b91b
huangbingbing12 <1390013437@qq.com>
2018-06-29 17:27:07 +0800

    first commit

---
+++ scripts/test_demo.py
@@ -0,0 +1,32 @@
+class TestDemo():
+    def test_demo(self):
+        print("吃饭")
+        assert 1
+    def test_demo1(self):
+        print("喝水")
+        assert 0
+
+    def test_demo3(self):
+        print("喝水")
+        assert 0
+
+    def test_demo4(self):
+        print("喝水")
+        assert 0
+
+    def test_demo5(self):
+        print("喝水")
+        assert 0
+
+    def test_demo6(self):
+        print("喝水")
+        assert 0
+
+    def test_demo7(self):
+        print("喝水")
+        assert 0
+
+    def test_demo2(self):
+        print("喝水")
+        assert 0
+
